ccf55a4db978db8d62f83276b2ab97b4f5b3548a
AshenMJ <72614990+AshenMJ@users.noreply.github.com>
2020-11-09 12:58:34 +0100

    zad1

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -7,10 +7,53 @@ MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
-    ui->setupUi(this);
-    srand(time(NULL));
-    int x=rand()%6;
-    ui->comboBox->setCurrentIndex(x);
+ ui->setupUi(this);
+ {
+ srand(time(NULL));
+
+        int los[6]={};
+
+                bool ok;
+                int il=0;
+                do
+                {
+                    int liczba = rand()%6;
+                    ok=true;
+
+                    for (int i = 0; i < il; i++)
+                    {
+                        if (liczba == los[i]) ok = false;
+                    }
+
+                    if (ok)
+                    {
+                        los[il] = liczba;
+                        switch(liczba){
+                        case 0:
+                            ui->comboBox->addItem("dodawanie");
+                            break;
+                        case 1:
+                            ui->comboBox->addItem("odejmowanie");
+                            break;
+                        case 2:
+                            ui->comboBox->addItem("mnozenie");
+                            break;
+                        case 3:
+                            ui->comboBox->addItem("dzielenie");
+                            break;
+                        case 4:
+                            ui->comboBox->addItem("potegowanie");
+                            break;
+                        case 5:
+                            ui->comboBox->addItem("pierwiastkowanie");
+                            break;
+                        }
+                        il++;
+
+                    }
+                } while (il!=6);
+    }
+
 
 }
 
